f9e5bfdf6dbe379acc56a49e0c77d7d63d576ae8
Alejandro Morente <morente5@hotmail.es>
2016-10-16 21:08:35 +0200

    Rewritten some code

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function rollDice(n=6) {
 
 function rollNDice(times=2, n=6) {
 	var roll = [];
-	for (var i = 0; i < times; i++) {
+	for (let i = 0; i < times; i++) {
 		roll[i]= rollDice(n);
 	}
 	return roll;
@@ -23,14 +23,11 @@ function rollsArray(rolls, times=2, n=6) {
 	return rollsArr;
 }
 
-// Rewrite
-function rollsValueArray(array) {
-	rollsValArr = [];
-	for (var i = 0; i < array.length; i++) {
-		roll = array[i];
-		rollsValArr[i] = roll.reduce(getSum);
-	}
-	return rollsValArr;
+
+function rollsValues(array) {
+	return array.map(function(element) {
+		return element.reduce(getSum);
+	});
 }
 
 
@@ -41,7 +38,7 @@ function getSum(total, num) {
 
 function frequency(array) {
 	var freq = {};
-	for (var i = 0; i < array.length; i++) {
+	for (let i = 0; i < array.length; i++) {
 		var element = array[i];
 		if (freq[element]) {
 			freq[element]++;
@@ -53,20 +50,32 @@ function frequency(array) {
 }
 
 
-function createTableRolls(parent, rolls, rollsValues) {
+function createTableRolls(parent, rolls) {
 	var table = document.createElement('table');
+	var rollsVal = rollsValues(rolls);
 
-	for (var i = 0; i < rolls.length; i++) {
+	var topRow = document.createElement('tr');
+	topRow.appendChild(document.createElement('td'));
+	topRow.cells[0].appendChild(document.createTextNode("Roll"));
+	for (let i = 0; i < rolls[0].length; i++) {
+		topRow.appendChild(document.createElement('td'));
+        topRow.cells[i + 1].appendChild(document.createTextNode("Dice " + (i + 1)));
+	}
+	topRow.appendChild(document.createElement('td'));
+	topRow.cells[rolls[0].length + 1].appendChild(document.createTextNode("Points"));
+	table.appendChild(topRow);
+
+	for (let i = 0; i < rolls.length; i++) {
         var row = document.createElement('tr');
         row.appendChild(document.createElement('td'));
-        row.cells[0].appendChild(document.createTextNode("Roll " + (i+1) + ":  "));
+        row.cells[0].appendChild(document.createTextNode(i + 1));
 
-        for (var j = 0; j < rolls[i].length; j++) {
+        for (let j = 0; j < rolls[i].length; j++) {
             row.appendChild(document.createElement('td'));
             row.cells[j + 1].appendChild(document.createTextNode(rolls[i][j]));
         }
         row.appendChild(document.createElement('td'));
-        row.cells[rolls[i].length + 1].appendChild(document.createTextNode("Points: " + rollsValues[i]));
+        row.cells[rolls[i].length + 1].appendChild(document.createTextNode(rollsVal[i]));
         table.appendChild(row);
     }
 	parent.appendChild(table);
@@ -74,7 +83,7 @@ function createTableRolls(parent, rolls, rollsValues) {
 
 
 var diceRolls = rollsArray(36000);
-var diceRollsValues = rollsValueArray(diceRolls);
+var diceRollsValues = rollsValues(diceRolls);
 
 console.log(frequency(diceRollsValues));
 console.log(frequency(diceRolls));
@@ -82,5 +91,5 @@ console.log(frequency(diceRolls));
 
 window.onload = function() {
 	var tablearea = document.getElementById('tablearea');
-	createTableRolls(tablearea, diceRolls, diceRollsValues);
+	createTableRolls(tablearea, diceRolls);
 };
